163e5d11f3c6f8be65b56e48e187682b65b83036
Rick <enrico.silterra@fireisborn.org>
2011-09-28 13:10:24 -0600

    correct paths

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // define the consumer key CK="xxxx";
 include("ck.php");
-  $title = $_GET['title']; 
+  $title = $_POST['title']; 
  echo <<<EOF
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -14,9 +14,32 @@ include("ck.php");
 <body>
 <div id="container">
 EOF;
+if ($_FILES['uploadedfile']['tmp_name']) {
+$uploaddir = '/home1/fireisbo/www/retract/upload/input/';
+$newfile = str_replace(" ", "_",basename($_FILES['uploadedfile']['name']));
+$uploadfile = $uploaddir . $newfile;
+$email = $_POST['email'];
+if (move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $uploadfile)) {
+    echo "File is valid, and was successfully uploaded.\n";
+    $fh = fopen($uploadfile,"r+");
+    while ( ($buffer = fgets($fh)) !== false) {
+      $data .= $buffer;
+    }
+    $data = "#__email:$email\n" . $data;
+    echo "data is $data";
+    fclose($fh);
+    $fh = fopen($uploadfile,"w+");
+    fwrite($fh,$data);
+    fclose($fh);
+} else {
+    echo "Possible file upload attack!\n";
+}
+exit(0);
+
+}
 if($title) {
-  $doi = $_GET['doi']; 
-  $pmid = $_GET['pmid']; 
+  $doi = $_POST['doi']; 
+  $pmid = $_POST['pmid']; 
   echo "<h2>" . "Duplication information for \"$title\""  . "</h2><br/>\n";
   $tit_enc = urlencode($title);
   $P="http://api.mendeley.com/oapi/documents/search/$tit_enc?consumer_key=$CK";
@@ -70,7 +93,8 @@ if($title) {
  }
 }
  echo <<<EOF
-<form action="index.php" method="get" class="niceform">
+<form enctype="multipart/form-data" action="index.php" method="POST" class="niceform">
+       <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
 	<fieldset>
     	<legend>Enter Mendeley Information</legend>
         <dl>
@@ -85,8 +109,14 @@ if($title) {
             <dt><label for="doi">DOI:</label></dt>
             <dd><input type="text" value="$doi" name="doi" id="doi" size="32" maxlength="128" /></dd>
         </dl>
+        <dl><dt>OR You can upload a file to process.  Supply an email address here.</dt><dd>
+        <input type="text" name="email" id="email" size="32" maxlength="128" />
+        </dd></dl>
+        <dl><dt><label>Choose a file to upload:</label></dt>
+        <dd><input name="uploadedfile" type="file" /></dd>
+        </dl>
         <dl><dt><dd>
-    	<input type="submit" name="submit" id="submit" value="Search" />
+    	<input type="submit" name="submit" id="submit" value="SUBMIT" />
         </dd></dt>
         </dl>
         <dl><dt>*:</dt><dd>Required</dd></dl>
